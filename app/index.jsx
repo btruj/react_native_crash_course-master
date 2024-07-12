36546c9e6c7aab0326e4486ebfc9053c06922f2a
Bryan Trujillo <trujillobryan99@yahoo.com>
2024-07-12 00:19:11 -0700

    added button component and index.jsx to display

--- app/index.jsx
+++ app/index.jsx
@@ -3,6 +3,7 @@ import {  Image, ScrollView, Text, View } from 'react-native';
 import { Link } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '../constants';
+import CustomButton from '../components/CustomButton';
 
 export default function App() {
   return (
@@ -37,7 +38,11 @@ export default function App() {
             Where Creativity Meets Innovation: Embark on a Journey of Limitless
             Exploration with Aora
           </Text>
-
+          <CustomButton 
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyles="w-full mt-7"
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
