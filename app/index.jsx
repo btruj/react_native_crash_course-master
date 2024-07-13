6ce0e2b73279ad3114d7a743e409600b9140be57
Bryan Trujillo <trujillobryan99@yahoo.com>
2024-07-12 21:57:43 -0700

    Added authlogout functionality & sign in page

--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import {  Image, ScrollView, Text, View } from 'react-native';
-import { Link } from 'expo-router';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import { Redirect, router } from "expo-router";
+import { View, Text, Image, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from '../constants';
 import CustomButton from '../components/CustomButton';
 
