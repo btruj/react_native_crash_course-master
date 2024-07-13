ed246baf205fc3292befb5b2b070b4e9b754a1f5
Bryan Trujillo <trujillobryan99@yahoo.com>
2024-07-13 11:42:58 -0700

    added image&functionality to password input

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -1,5 +1,6 @@
-import { View, Text, TextInput } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native'
 import React, { useState } from 'react'
+import { icons } from '../constants'
 
 const FormField = ({ title, value, placeholder, handleChangeText,otherStyles, ...props}) => {
     const [showPassword, setShowPassword] = useState(false)
@@ -17,7 +18,15 @@ const FormField = ({ title, value, placeholder, handleChangeText,otherStyles, ..
         onChangeText={handleChangeText}
         secureTextEntry={title === 'Password' && !showPassword} {...props}
         />
-
+       {title === 'Password' &&(
+         <TouchableOpacity onPress={() =>
+          setShowPassword(!showPassword)}>
+            <Image source={!showPassword ? icons.eye : icons.eyehide } className="w-6 h-6"
+            resizeMode='contain'/>
+          </TouchableOpacity>
+         
+         
+       )}
      </View>
     </View>
   )
